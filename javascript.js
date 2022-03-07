f4d42d5ceb9533450e33272ab86a182ee0bfb198
xabnzk <93670973+xabnzk@users.noreply.github.com>
2022-03-07 19:01:27 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var input_Giris = prompt("Giris Miladi:");
+
+var ilk_asama = input_Giris-621;
+var ikinci_asama = ilk_asama/33;
+var ucuncu_asama = ilk_asama+ikinci_asama;
+
+console.log(ucuncu_asama)
+
+alert("Sonuc :"+ ucuncu_asama.toFixed(0))
